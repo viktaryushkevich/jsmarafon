fa8a9e4672fcdf9a25cc8a9f46aae07178b91980
Viktor Yushkevich <viktor.yushkevich@vrpconsulting.com>
2020-08-25 23:03:27 +0300

    feat new updates

--- main.js
+++ main.js
@@ -1,60 +1,76 @@
 const $btn = document.getElementById('btn-kick');
+const $btnSuper = document.getElementById('btn-punch');
 
 const character = {
-  name: 'Picachu',
-  defaultHP: 100,
-  damageHP: 90,
-  elHP: document.getElementById('health-character'),
-  elProgressbar: document.getElementById('progress-character'),
+    name: 'Pikcahu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
 }
 
 const enemy = {
-  name: 'Charmander',
-  defaultHP: 100,
-  damageHP: 100,
-  elHP: document.getElementById('health-enemy'),
-  elProgressbar: document.getElementById('progress-enemy'),
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
 }
 
 $btn.addEventListener('click', function () {
-  console.log('Kick');
-  changeHP(random(20), character);
-  changeHP(random(20), enemy);
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+
+$btnSuper.addEventListener('click', function () {
+    changeHP(random(10), character);
+    changeHP(random(50), enemy);
 });
 
 function init() {
-  console.log('Start game!');
-  renderHP(character);
-  renderHP(enemy);
-};
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+}
 
 function renderHP(person) {
-  renderHPLife(person);
-  renderProgressbarHP(person);
+    renderHPLife(person);
+    renderProgressbarHP(person);
 }
 
 function renderHPLife(person) {
-  person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
 }
 
 function renderProgressbarHP(person) {
- person.elProgressbar.style.width = person.damageHP+ '%';
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function superHit() {
+    if (character.damageHP < 50) {
+        $btnSuper.style.display = 'block';
+        $btnSuper.onclick = function () {
+            $btnSuper.disabled = true;
+        }
+    }
+
 }
 
 function changeHP(count, person) {
-  if (percon.damageHP >count) {
-    person.damageHP = 0;
-    alert('Бедный' + person.name + 'проиграл бой!')
-    $btn.disabled.true;
-  } else {
-    person.damageHP += count
-  }
-
-  renderHP(person);
+    if (person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+        $btnSuper.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+    superHit();
+    renderHP(person);
 }
 
 function random(num) {
-  return Math.ceil(Math.random() * num);
+    return Math.ceil(Math.random() * num);
 }
 
 init();
